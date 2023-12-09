cb961a7adc7958813c2e19c53ea99fc2e6f3dd0e
Aleksander Gasior <aleksandergasior@MacBook-Air-Aleksander.local>
2023-12-09 13:57:30 +0100

    feature: create layout

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,19 @@
+import logo from '../../images/logo.svg';
+
+type LayoutType = {
+  children: JSX.Element;
+};
+const Layout = ({ children }: LayoutType) => {
+  return (
+    <>
+      <div className="ui one column center aligned page grid">
+        <div className="column twelve wide">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+      {children}
+    </>
+  );
+};
+
+export default Layout;
